ee4d72498628ca42eb3c8fa135f3775b7579b1eb
Matt Quinto <mfifth@Matts-MacBook.local>
2018-11-10 11:38:32 +1300

    added font awesome, sortable table columns

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,41 @@
 class HomeController < ApplicationController
-  def index
+  helper_method :sort_column, :sort_direction
 
+  def index
+    CsvRecord.destroy_all
   end
 
   def process_records
+    file = params[:csv_file].path
 
+    CSV.foreach(file, headers: true, header_converters: lambda {|header| header.downcase.gsub(" ", "_")}) do |row|
+      data = row.to_hash
+      CsvRecord.create(name: data['name'], record_date: data['date'], record_id: data['number'], description: data['description'])
+    end
+
+    redirect_to '/results'
   end
 
   def results
+    @csv_records = CsvRecord.order("#{sort_column} #{sort_direction}")
+  end
 
+  def search
+    query = params[:query]
+    puts query
+  end
+
+  private
+
+  def sortable_columns
+    ["name", "record_id", "record_date"]
+  end
+
+  def sort_column
+    sortable_columns.include?(params[:column]) ? params[:column] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
